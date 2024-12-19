632f29e797d9a10d19da8cbb50b320843757f9db
Emsal İrem <irem09847@gmail.com>
2024-12-19 17:03:24 +0300

    Emsal İrem Demirel

--- hikaye.c
+++ hikaye.c
@@ -6,10 +6,10 @@ int main() {
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
   printf("Bu bir örnektir uygulamayızı kendi satırınıza alt satıra inmeden bu şekil yazın ");          //ÖRNEK (adın soyadın işte :d ); SOL TARAFINA YAZACAKSINIZ
           //iREM TOKTAŞ
-          // EMSAL İREM DEMİREL
+  printf("Kalpleri korkuyla hızla çarparken, yavaş yavaş karanlığa doğru ilerliyorlardı. Abisi, kardeşinin elini sıkıca tuttu ve ' Korkma, ben senin yanındayım.' diye fısıldadı.")  // EMSAL İREM DEMİREL
           //İREM TOPSAKAL
           //Mustafa Saput
-        printf("malzemeyi arap faikten aldım karabükte ikinci yükleme yapılıyordu adanaya kadarda ben kullanıyordum kamyonu..");  //Yasin ERGÜT
+          //Yasin ERGÜT
           //melike yilmaz
           //Ayşe Nur Karakoyunlu
           //elif zeren boyraz
